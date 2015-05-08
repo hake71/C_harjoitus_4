1dc319f3c61c6189fcd9b8e6d0ae7eaab1fa7a73
hake71 <hannu.harju@hotmail.com>
2015-05-08 12:01:53 +0300

    Final version

--- laskin.c
+++ laskin.c
@@ -18,49 +18,39 @@
 	*
 	*/
 
+/*	Convert string to integer,
+	if string is not number, return 0 as error */
 char Is_Number(char *mjono, int *number)
 {
 	unsigned char  i = 0;
 	int Luku = 0;
+	/* Convert string to integer */
 	
+	/* Go through full string */
 	while(mjono[i]){
+		/* If digit ... */
 		if(isdigit(mjono[i]))
 		{
-	//	printf("%c", mjono[i]);
+			/* ...Add numeric value to result */
 			Luku = Luku *10 + (mjono[i] - '0');
+		/* If not digit ... */
 		}else {
+			/* Return error code */
 			return 0;
 		}
-		
+		/* Update index for next char */
 		i++;
 	}
+	
+	/* Result number */
 	*number = Luku;
-//	printf("  -> %d \n", Luku);
+	/* return OK code */
 	return 1;
 }
 
-char Is_Operator(char *mjono, char *oper)
+/* 	Check if correct operand */
+char Is_Operator(char *mjono)
 {
-	switch((char)mjono[0]){
-		case '+': *oper = -1;
-				break;
-		case '-': *oper = -2;
-				break;
-		case 'x': 				
-		case '*': *oper = 1;
-				break;
-		case '/': *oper = 2;
-				break;
-		default:
-			*oper = 0;
-			return 0;
-	}
-	return 1;
-}
-
-char Is_Operator2(char *mjono)
-{
-#if 1
 	switch((char)mjono[0]){
 		case '+':
 		case '-':
@@ -72,31 +62,16 @@ char Is_Operator2(char *mjono)
 		default:
 			return 0;
 	}
-
-#else
-	switch((char)mjono[0]){
-		case '+': *oper = '+';
-				break;
-		case '-': *oper = '-';
-				break;
-		case 'x': 				
-		case '*': *oper = 'x';
-				break;
-		case '/': *oper = '/';
-				break;
-		default:
-			*oper = 0;
-			return 0;
-	}
-#endif
 	return 1;
 }
 
-
+/* Define number and operand tables */
 #define MAX_PARAM 10
 double Numbers[MAX_PARAM+1];
 char Operators[MAX_PARAM];
 
+/* 	Remove number and operand when calculation
+	is executed */
 void Remove_index(char index){
 	unsigned char i;
 	for( i = index; i < (MAX_PARAM-1); i++)
@@ -106,21 +81,9 @@ void Remove_index(char index){
 	}
 }
 
+/* 	Execute calculation based on operand and
+ 	and return result */
 double calculate(double luku1, double luku2, char oper){
-
-	switch(oper)
-	{
-		case -1: return luku1 + luku2;
-		case -2: return luku1 - luku2;
-		case 1: return luku1 * luku2;
-		case 2: return luku1 / luku2;
-		default:
-			printf("!! ERROR in count !!!!\n");
-	}
-	return 0;
-}
-
-double calculate2(double luku1, double luku2, char oper){
 
 	switch(oper)
 	{
@@ -134,102 +97,83 @@ double calculate2(double luku1, double luku2, char oper){
 	return 0;
 }
 
-
-void print_status(char count)
-{
-	unsigned char i;
-	printf(" O: ");
-	for(i=0;i<(count+1);i++){
-		printf(" %5d", Operators[i] );
-	}
-	printf("\n P: ");
-	for(i=0;i<(count+1);i++){
-	printf(" %1.2f", Numbers[i] );
-	}
-	printf("\n");
-}
-
-void print_status2(char count)
-{
-	unsigned char i;
-	printf(" O: ");
-	for(i=0;i<(count+1);i++){
-		printf(" %5c", Operators[i] );
-	}
-	printf("\n P: ");
-	for(i=0;i<(count+1);i++){
-	printf(" %1.2f", Numbers[i] );
-	}
-	printf("\n");
-}
-
+/* 	Compare current and next operand order:
+	If first operand is "+" or "-" and next
+	operand is '*' or '/', then next operand have
+	higer priority asn is executed -> Result = 1 
+	If current operand is same or higer priority
+	than next one, then execute current operand
+	-> Result = 0  */
 char CheckCountOrder(char op1, char op2){
-
-	if(((op2 == 'x') ||(op2 == '/' )) && ( (op1 == '+') ||(op1 == '-' ))){
+	/* If next operand have higer priority .. */
+	if(((op2 == 'x') ||(op2 == '/' )) && \
+		((op1 == '+') ||(op1 == '-' ))){
+		/* Return 1 */
 		return 1;
 	}
+	/* Otherwise return 0 */
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
 
 	int param;
-	char oper;
 	unsigned int i;	
-	unsigned char o_count, p_count;
+	unsigned char o_count, n_count;
 	
-	if(argc == 1)
-	{
+	/* Check arguments */
+	/* There is no arguments */
+	if(argc == 1){
 		printf(" Error: No parameters\n");
 		return 1;
+		
+	/* There is 2 arguments */
 	} else if(argc == 2){
-		printf(" %s =  %s\n", argv[1], argv[1]);
+		printf(" %s = %s\n", argv[1], argv[1]);
 		return 0;
 	}
+	
+	/* Initialize operand and number count */ 
  	o_count = 0;
- 	p_count = 0;
+ 	n_count = 0;
  
 	/* Check parameters */
 	for(i = 1; i < argc; i++){
-
-	/* Should be number */
+		/* Should be number */
 		if(i & 1){
-		//	printf(" N: %s -> ", argv[i]);
+			/* Check if parameter is number ... */
 			if(Is_Number(argv[i], &param)){
-			//	printf(" Luku: %d \n", param);
-				Numbers[p_count++] = param;
+				/* ... parameter is number */
+				Numbers[n_count++] = param;
 			 } else {
+				/* ... parameter is NOT number */
 			 	printf(" ERROR: %s not number\n", argv[i]);
 			 	return 1;
 			 }
 		}
 		/* Should be operator */
 		else {
-	//		printf(" O: %s -> ", argv[i]);
-			if(Is_Operator2(argv[i])){
-		//	printf(" Operator: %d \n", oper);
-			Operators[o_count++] = (char)argv[i][0];
+			/* Check if parameter is operator ... */
+			if(Is_Operator(argv[i])){
+				/* ... parameter is operator */
+				Operators[o_count++] = (char)argv[i][0];
 			}else {
+				/* ... parameter is NOT operator */
 			 	printf(" ERROR: %s not Operator\n", argv[i]);
 			 	return 1;
-			 }		
+			}		
 		}		
 	} /* for() */
 	
-//	printf(" o count %d <-> %d p count\n", o_count, p_count);
-	if((o_count + 1) != p_count)
-	{
-		printf(" Error in formula \n");
+	/* Final check: Compare number and operator amount */
+	if((o_count + 1) != n_count){
+		printf("ERROR: Operator and number count not match \n");
 		return 1;
 	}
 
-//	double tulos = 0;
-//	double tmp_tulos = 0;
-// HH	print_status2(o_count);
 	/* Print calculus */
 	printf(" %d",(int)Numbers[0]);
-	for(i=0;i<o_count;i++)
-	{
+	for(i=0;i<o_count;i++){
 		printf(" %c",Operators[i]);
 		printf(" %d",(int)Numbers[i+1]);
 	}
@@ -237,26 +181,25 @@ int main(int argc, char *argv[]) {
 	
 	/* Actual calculation */
 	do{
-		/* Next operand have higer priority */
-//		if((Operators[0] < 0) && (Operators[1]>0))
-		if(CheckCountOrder(Operators[0], Operators[1]))
-		{
-			Numbers[1] = calculate2(Numbers[1], \
+		/* If next operand have higer priority ... */
+		if(CheckCountOrder(Operators[0], Operators[1])){
+			/* Calculate next operand  */
+			Numbers[1] = calculate(Numbers[1], \
 				Numbers[2], Operators[1]);
 			Remove_index(1);
 		}
-		/* Current can calculate */
+		/* Can calculate current operand ... */
 		else{
-			Numbers[0] = calculate2(Numbers[0], \
+			/* Calculate current operand */
+			Numbers[0] = calculate(Numbers[0], \
 				Numbers[1], Operators[0]);
 			Remove_index(0);
 		}
-
 		o_count--;
-	// HH	print_status2(o_count);
 	}while(o_count);
-	printf(" %.2f\n", Numbers[0]);
 	
+	/* Print calculation result */
+	printf(" %.2f\n", Numbers[0]);
 	
 	return 0;
 }
